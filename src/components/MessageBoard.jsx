542368deecb7a4875107f02bd42d9f12834f4ad7
parinTNK <parin.tnk@gmail.com>
2025-03-24 13:14:21 +0700

    submit

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,25 +1,55 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [messages, setMessages] = useState([]);
+  const [messageText, setMessageText] = useState("");
+
+  const handleInputChange = (event) => {
+    setMessageText(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();  
+      setMessages([...messages, messageText]);
+      setMessageText("");
+  };
+
+  const handleDeleteMessage = (index) => {
+    setMessages(messages.filter((_, i) => i !== index));
+  };
+
   return (
-    <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+    <form className="app-wrapper" onSubmit={handleFormSubmit}>
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={messageText}
+            onChange={handleInputChange}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" type="submit">
+          Submit
+        </button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+        {messages.map((message, index) => (
+          <div key={index} className="message" style={{  }}>
+            <h1>{message}</h1>
+            <button
+              className="delete-button"
+              onClick={() => handleDeleteMessage(index)}
+            >
+              x
+            </button>
+          </div>
+        ))}
       </div>
-    </div>
+    </form>
   );
 }
 
